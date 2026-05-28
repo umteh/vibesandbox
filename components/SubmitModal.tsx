6f40392ef4b0c1498434e14b9a1596d599f14925
umteeh <umteeh@gmail.com>
2026-05-28 17:23:36 -0400

    cleanup: remove score-listing debug console.log

--- components/SubmitModal.tsx
+++ components/SubmitModal.tsx
@@ -146,9 +146,7 @@ export default function SubmitModal({ onClose, onSubmitListing, user, supabaseUs
           description: form.description,
           platform: form.platform,
         }),
-      })
-        .then(r => console.log('[score-listing] status:', r.status, 'listing:', data.id))
-        .catch(err => console.error('[score-listing] fetch error:', err));
+      }).catch(() => {});
 
       onSubmitListing({
         id: data.id,
